6dbcf899817884d35d9e6f23c409eaa24e4126da
Gabriel Joioso <gjoioso.cs@gmail.com>
2021-04-29 15:35:33 -0300

    FileWritter, BufferedWriter e Manipulando pastas com file

---
+++ filewriter-bufferedwriter/src/application/Program.java
@@ -0,0 +1,29 @@
+// Cria o arquivo e grava dentro dele
+
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night"};
+		
+		String path = "c:\\temp\\out.txt";
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) { // true significa que você não quer recriar o arquivo
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
